a097ba9e8c8f4a11b80023861c4eb1197ee67366
jww <jww@localhost>
2006-03-12 18:51:59 -0800

    Xcode-compatible includes.

--- Source/CFStringToUTF8.h
+++ Source/CFStringToUTF8.h
@@ -1,7 +1,12 @@
 #pragma once
 
-#include <CFBase.h>
-#include <stringfwd>
+#if __MACH__
+	#include <CoreFoundation/CoreFoundation.h>
+#else
+	#include <CFBase.h>
+#endif
+
+#include <string>
 
 std::string		CFStringToUTF8( CFStringRef inStr,
 	CFStringEncoding inEncoding = kCFStringEncodingUTF8 );
