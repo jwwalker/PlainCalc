7af78e899e1aab186ad1c6154675d18ba475bb29
James Walker <jamesw@frameforge3d.com>
2006-03-15 22:51:56 -0800

    start

---
+++ Cocoa source/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  PlainCalc2
+//
+//  Created by James Walker on 3/11/06.
+//  Copyright James W. Walker 2006 . All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **) argv);
+}
